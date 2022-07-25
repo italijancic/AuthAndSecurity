64b6ad6ed6612f0516280924b4e684bdaf444c1f
italijancic <italijancic@outlook.com>
2022-07-25 20:13:24 -0300

    Edit pages paths to consume API users

--- configs/db.config.js
+++ configs/db.config.js
@@ -4,8 +4,9 @@ exports.dbConnect = async() => {
 
 	try {
 
+		console.log('[configs/db.config.js]: ⏳ Connecting DB')
 		await mongoose.connect(process.env.DB_URL)
-		console.log('[configs/db.config.js]: DB Succesfully connected')
+		console.log('[configs/db.config.js]: ✅ DB Succesfully connected')
 
 	} catch (error) {
 		console.log(error)
